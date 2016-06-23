f80acf77bca7cd06676aa796ab3d9c97affe812f
qilong1980 <er_shi@163.com>
2016-06-23 11:13:59 +0800

    bbs

---
+++ app01/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+class Category(models.Model):
+    pass
+
+class Username(models.Model):
+    pass
+
+class Usergroup(models.Model):
+    pass
